58daf8c8ae31bea2c33146c9505f2fa63baf6a29
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-11-11 20:43:15 -0500

    Added simple Todo Class

--- MAPD714-F2021-Week9/ViewController.swift
+++ MAPD714-F2021-Week9/ViewController.swift
@@ -1,19 +1,51 @@
-//
-//  ViewController.swift
-//  MAPD714-F2021-Week9
-//
-//  Created by Tom Tsiliopoulos on 2021-11-11.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource
+{
+      
+    var todos =
+    [Todo(name: "First Todo"),
+     Todo(name: "Second Todo"),
+     Todo(name: "Third Todo")]
+    
+    let tableIdentifier = "todosTable"
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //displayTodos()
     }
-
+    
+    func displayTodos()->Void
+    {
+        print("Todo List")
+        print("-------------------------------")
+        for todo in todos
+        {
+            todo.display()
+            print("-------------------------------")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        var cell = tableView.dequeueReusableCell(withIdentifier: tableIdentifier)
+        if(cell == nil)
+        {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: tableIdentifier)
+        }
+        
+        cell?.textLabel?.text = todos[indexPath.row].name
+        
+        return cell!
+    }
 
 }
 
